e0777ca52e255b9937261a19a82817e839fe2f1f
daaniel32 <daaniel3232@gmail.com>
2020-06-20 16:46:15 -0300

    Condições(part.1)

---
+++ aula_11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do deu carro é ${vel}km/h`)
+if (vel > 60) {
+     console.log(`Você ultrapassou a velocidade permitida> Multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
