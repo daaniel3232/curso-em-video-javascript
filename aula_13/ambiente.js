0ad70d5edb56f0ffdf47445c626c0136d4b2c39f
daniel silva <63619194+daaniel3232@users.noreply.github.com>
2020-07-07 21:00:50 -0300

    Estruturas de Repetições(part.1)

---
+++ aula_13/ambiente.js
@@ -0,0 +1,25 @@
+var c =0
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+
+
+
+
+
+// estrutura de repetiçoes com teste lógico no começo
+// var c = 0 
+// while( c <= 500) {
+//     console.log(`Passo ${c}`)
+//     c++ // c = c + 1
+// }
+
+
+
+// console.log('TUdo Bem?')
+// console.log('TUdo Bem?')
+// console.log('TUdo Bem?')
+// console.log('TUdo Bem?')
+// console.log('TUdo Bem?')
+// console.log('TUdo Bem?')
